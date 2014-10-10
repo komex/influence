34f63783898561d51b18ae449d3054e6cdb8e86a
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-10-10 17:27:44 +0400

    Add testRemoveControl().

--- tests/RemoteControlTest.php
+++ tests/RemoteControlTest.php
@@ -37,4 +37,29 @@ class RemoteControlTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(RC::isUnderControl($object1));
         $this->assertFalse(RC::isUnderControl($object2));
     }
+
+    /**
+     * Test removing object from control.
+     */
+    public function testRemoveControl()
+    {
+        $className = 'SomeClassName';
+        $object1 = new \stdClass();
+        $object2 = new \stdClass();
+
+        RC::control($className);
+        RC::control($object1);
+        RC::control($object2);
+
+        $this->assertTrue(RC::isUnderControl($className));
+        $this->assertTrue(RC::isUnderControl($object1));
+        $this->assertTrue(RC::isUnderControl($object2));
+
+        RC::removeControl($className);
+        RC::removeControl($object1);
+
+        $this->assertFalse(RC::isUnderControl($className));
+        $this->assertFalse(RC::isUnderControl($object1));
+        $this->assertTrue(RC::isUnderControl($object2));
+    }
 }
