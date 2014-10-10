ede9c496b7eb86304174762d6a531cf7de34569d
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-10-10 17:49:57 +0400

    Fix Manifest::registerCall(). Add Manifest::clearCalls(). Add ManifestTest::testRegisterCall(). Add ManifestTest::testClearRegisteredCalls().

--- tests/ManifestTest.php
+++ tests/ManifestTest.php
@@ -31,5 +31,40 @@ class ManifestTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($manifest->intercept('method1'));
         $this->assertFalse($manifest->intercept('method2'));
     }
+
+    /**
+     * Test calls registration works fine.
+     */
+    public function testRegisterCall()
+    {
+        $manifest = new Manifest();
+        $this->assertInternalType('array', $manifest->getCalls());
+        $this->assertEmpty($manifest->getCalls());
+
+        $manifest->registerCall('method', []);
+        $this->assertEmpty($manifest->getCalls());
+
+        $manifest->registerCalls(true);
+        $manifest->registerCall('method', []);
+        $manifest->registerCall('method2', ['args' => true]);
+        $this->assertSame([['method', []], ['method2', ['args' => true]]], $manifest->getCalls());
+
+        $manifest->registerCalls(false);
+        $manifest->registerCall('method3', []);
+        $this->assertSame([['method', []], ['method2', ['args' => true]]], $manifest->getCalls());
+    }
+
+    /**
+     * Test we can clear methods calls.
+     */
+    public function testClearRegisteredCalls()
+    {
+        $manifest = new Manifest();
+        $manifest->registerCalls(true);
+        $manifest->registerCall('method', []);
+        $this->assertNotEmpty($manifest->getCalls());
+        $manifest->clearCalls();
+        $this->assertEmpty($manifest->getCalls());
+    }
 }
  
