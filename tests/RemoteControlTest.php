78f2eeb01ef18a5c4e607e52e569a5921a8e5904
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-10-10 17:25:17 +0400

    Add testIsUnderControl().

---
+++ tests/RemoteControlTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is a part of influence project.
+ *
+ * (c) Andrey Kolchenko <a.j.kolchenko@baltsoftservice.ru>
+ */
+
+namespace Test\Influence;
+
+use Influence\RemoteControl as RC;
+
+/**
+ * Class RemoteControlTest
+ *
+ * @package Test\Influence
+ * @author Andrey Kolchenko <a.j.kolchenko@baltsoftservice.ru>
+ */
+class RemoteControlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test checking object status.
+     */
+    public function testIsUnderControl()
+    {
+        $className = 'SomeClassName';
+        $object1 = new \stdClass();
+        $object2 = new \stdClass();
+
+        $this->assertFalse(RC::isUnderControl($className));
+        $this->assertFalse(RC::isUnderControl($object1));
+        $this->assertFalse(RC::isUnderControl($object2));
+
+        RC::control($className);
+        RC::control($object1);
+
+        $this->assertTrue(RC::isUnderControl($className));
+        $this->assertTrue(RC::isUnderControl($object1));
+        $this->assertFalse(RC::isUnderControl($object2));
+    }
+}
