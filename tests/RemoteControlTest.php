979587548202adde22fb6bbbaf271978444239c1
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-10-28 12:19:53 +0300

    Add testControlStaticClassName() and testControlStaticInvalidArgument().

--- tests/RemoteControlTest.php
+++ tests/RemoteControlTest.php
@@ -31,4 +31,42 @@ class RemoteControlTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(self::SIMPLE_CLASS_NAME . '::method', $class->method());
         $this->assertSame(self::SIMPLE_CLASS_NAME . '::staticMethod', $class->staticMethod());
     }
+
+    /**
+     * Test control static methods with specify class name.
+     */
+    public function testControlStaticClassName()
+    {
+        $this->assertFalse(RC::isUnderControlStatic(self::SIMPLE_CLASS_NAME, 'staticMethod'));
+        RC::controlStatic(self::SIMPLE_CLASS_NAME)->setReturn('staticMethod', 5);
+        $this->assertSame(5, SimpleClass::staticMethod());
+        RC::controlStatic('\\' . self::SIMPLE_CLASS_NAME)->setReturn('staticMethod', 6);
+        $this->assertSame(6, SimpleClass::staticMethod());
+    }
+
+    /**
+     * @return array
+     */
+    public function dpControlStaticInvalidArgument()
+    {
+        return [
+            [5],
+            [[1 => true]],
+            [.2],
+            ['NotExistentClassName']
+        ];
+    }
+
+    /**
+     * Test incoming arguments for controlStatic.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Target must be an object or string of class name.
+     *
+     * @dataProvider dpControlStaticInvalidArgument
+     */
+    public function testControlStaticInvalidArgument($target)
+    {
+        RC::controlStatic($target);
+    }
 }
