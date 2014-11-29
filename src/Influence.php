f4792ec8cd342a21218b0bda06b674f19f4209de
Andrey Kolchenko <komexx@gmail.com>
2014-11-30 02:27:02 +0300

    Remove autoload from bootstrap file. Refactor Influence, add ability to skip classes starts with specified namespace.

--- src/Influence.php
+++ src/Influence.php
@@ -25,67 +25,88 @@ final class Influence
      * @var ClassLoader
      */
     private $composer;
+    /**
+     * @var array
+     */
+    private $blackList = [];
 
     /**
-     * Init
+     * Init Influence.
+     *
+     * @param array $blackList
      */
-    private function __construct()
+    private function __construct(array $blackList)
     {
-        $this->composer = $this->getComposer();
-        $this->registerModules();
+        $this->replaceAutoLoaders();
+        array_push($blackList, __NAMESPACE__);
+        foreach ($blackList as $class) {
+            $this->addToBlackList($class);
+        }
     }
 
     /**
      * Integrate Influence in code.
+     *
+     * @param array $blackList
      */
-    public static function affect()
+    public static function affect(array $blackList = [])
     {
         if (self::$instance === null) {
-            self::$instance = new self();
+            self::$instance = new self($blackList);
+        }
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return bool|null True if loaded, null otherwise
+     */
+    public function loadClass($class)
+    {
+        $class = ltrim($class, '\\');
+        foreach ($this->blackList as $namespace) {
+            if (strpos($class, $namespace) === 0) {
+                return $this->composer->loadClass($class);
+            }
+        }
+        $file = $this->composer->findFile($class);
+        if ($file !== false) {
+            \Composer\Autoload\includeFile('php://filter/read=influence.reader/resource=' . $file);
+
+            return true;
+        } else {
+            return null;
         }
     }
 
     /**
      * @param string $class
      */
-    public function loadClass($class)
+    private function addToBlackList($class)
     {
-        if (strpos(ltrim($class, '\\'), __NAMESPACE__) === 0) {
-            $this->composer->loadClass($class);
-        } else {
-            $file = $this->composer->findFile($class);
-            if ($file !== false) {
-                \Composer\Autoload\includeFile('php://filter/read=influence.reader/resource=' . $file);
-            }
+        $class = ltrim($class, '\\');
+        if (in_array($class, $this->blackList) === false) {
+            array_push($this->blackList, $class);
         }
     }
 
     /**
      * Register Influence loader and filter.
      */
-    private function registerModules()
-    {
-        $loaders = spl_autoload_functions();
-        foreach ($loaders as $loader) {
-            spl_autoload_unregister($loader);
-        }
-        spl_autoload_register([$this, 'loadClass']);
-        stream_filter_register('influence.reader', 'Influence\\Injector');
-    }
-
-    /**
-     * @return ClassLoader
-     * @throws \RuntimeException
-     */
-    private function getComposer()
+    private function replaceAutoLoaders()
     {
         $loaders = spl_autoload_functions();
         foreach ($loaders as $loader) {
             list($object) = $loader;
-            if (get_class($object) === 'Composer\\Autoload\\ClassLoader') {
-                return $object;
+            if ($object instanceof ClassLoader) {
+                $this->composer = $object;
             }
+            spl_autoload_unregister($loader);
         }
-        throw new \RuntimeException('Composer autoloader not registered');
+        if ($this->composer === null) {
+            throw new \RuntimeException('Composer autoloader not registered');
+        }
+        spl_autoload_register([$this, 'loadClass']);
+        stream_filter_register('influence.reader', 'Influence\\Injector');
     }
 }
